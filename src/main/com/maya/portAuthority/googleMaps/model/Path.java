de34b9bd431a6012a0f6fc2c8cb05e3e7e4d6575
Jonathan Brown <jonathan.h.brown@gmail.com>
2017-01-23 15:39:05 -0500

    Merged with GoogleMaps/model

---
+++ src/main/com/maya/portAuthority/googleMaps/model/Path.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.maya.portAuthority.googleMaps.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author Adithya
+ */
+public class Path {
+
+    List<Point> path; //path: list of points
+    int distance; //length of path
+    int duration; //duration to traverse the path
+    String travelMode; //travel mode of the path
+    String htmlText;//HTML instruction associated with the path.
+
+    /**
+     * @param path The list of points that make the path
+     */
+    public Path(List<Point> path) {
+        super();
+        this.path = path;
+    }
+
+    /**
+     * @return path
+     */
+    public final List<Point> getPath() {
+        return path;
+    }
+
+    /**
+     * @param path to set
+     */
+    public final void setPath(List<Point> path) {
+        this.path = path;
+    }
+
+    /**
+     * @return the mDistance
+     */
+    public final int getDistance() {
+        return distance;
+    }
+
+    /**
+     * @return duration
+     */
+    public final int getDuration() {
+        return duration;
+    }
+
+    /**
+     * @return travelMode
+     */
+    public final String getTravelMode() {
+        return travelMode;
+    }
+
+    /**
+     * @return HTML text
+     */
+    public final String getHtmlText() {
+        return htmlText;
+    }
+
+    /**
+     * @param path to set
+     */
+    public final void setmPath(List<Point> path) {
+        this.path = path;
+    }
+
+    /**
+     * @param distance to set
+     */
+    public final void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    /**
+     * @param duration to set
+     */
+    public final void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    /**
+     * @param travelMode to set
+     */
+    public final void setTravelMode(String travelMode) {
+        this.travelMode = travelMode;
+    }
+
+    /**
+     * @param htmlText to set
+     */
+    public final void setHtmlText(String htmlText) {
+        this.htmlText = htmlText;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder strB = new StringBuilder();
+        strB.append(getHtmlText().replaceAll("\\<.*?\\> ?", " "));
+        return strB.toString();
+    }
+
+    /**
+     * Gives results of coordinates in the path. This can be used for plotting
+     * path on the map.
+     *
+     * @return
+     */
+    public String printPoints() {
+        StringBuilder strB = new StringBuilder();
+        for (Point point : path) {
+            strB.append(point.toString());
+            strB.append(point.toString());
+            strB.append(",");
+        }
+        return strB.toString();
+    }
+
+}
--- src/main/com/maya/portAuthority/util/OutputHelper.java
+++ src/main/com/maya/portAuthority/util/OutputHelper.java
@@ -4,30 +4,71 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.Image;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
 import com.amazon.speech.ui.SsmlOutputSpeech;
+import com.amazon.speech.ui.StandardCard;
 import com.maya.portAuthority.GetNextBusSpeechlet;
 import com.maya.portAuthority.storage.PaInputData;
 
 
 
 public class OutputHelper {
-	private static String SPEECH_WELCOME = "Welcome to Pittsburgh Port Authority ";
+	private static String SPEECH_WELCOME = "Welcome to "+GetNextBusSpeechlet.INVOCATION_NAME;
 
 	public static final String AUDIO_WELCOME = "<audio src=\"https://s3.amazonaws.com/maya-audio/ppa_welcome.mp3\" />";
 	private static final String AUDIO_FAILURE = "<audio src=\"https://s3.amazonaws.com/maya-audio/ppa_failure.mp3\" />";
 	private static final String AUDIO_SUCCESS = "<audio src=\"https://s3.amazonaws.com/maya-audio/ppa_success.mp3\" />";
 	
-	//TODO: replace string building with String format. Like the below.
-	//String template = "Hello %s Please find attached %s which is due on %s";
-	//String message = String.format(template, name, invoiceNumber, dueDate);
-	
 	//TODO: add markers into conversation
 	private static final String CHANGE_MARKER=" <break time=\"0.5s\" /> By the way, ";
 	private static final String SUCCESS_MARKER="okay, ";
 	private static final String FAILED_MARKER="oh, ";
+	
+	/**
+	 * Location Name, StopName
+	 */
+	private static final String LOCATION_SPEECH="The nearest stop to %s is %s. ";
+	/**
+	 * StopName
+	 */
+	private static final String BUSSTOP_SPEECH=" At %s, ";
+	
+	
+	////RESULTS////
+	/**
+	 * Speech fragment if there are no prediction results for an "All Routes" request
+	 * Format with Direction, BusStopName
+	 */
+	private static final String NO_ALL_ROUTES_SPEECH=" No %s busses are expected at %s in the next 30 minutes. ";
+	
+	/**
+	 * Speech fragment if there are no prediction results for an "All Routes" request
+	 * Format with Direction, RouteID, and BusStopName
+	 */
+	private static final String NO_SINGLE_ROUTE_SPEECH=" No %s, %s is expected at %s in the next 30 minutes. ";
+	
+	/**
+	 * Speech fragment for first prediction result
+	 * Format with RouteID, Prediction Time
+	 */
+	private static final String FIRST_RESULT_SPEECH=" The %s will be arriving in %s minutes ";
+	
+	/**
+	 * Speech fragment for additional prediction result
+	 * Format with Prediction Time
+	 */
+	private static final String MORE_RESULTS_SPEECH=" and %s minutes ";
+	
+	/**
+	 * Speech fragment with instructions to hear all routes.
+	 */
+	private static final String HELP_ALL_ROUTES_SPEECH=CHANGE_MARKER+"to hear predictions for all routes that stop there, say <break time=\"0.25s\" /> Alexa, ask "+GetNextBusSpeechlet.INVOCATION_NAME+" for All Routes";
+
+	
+
 
 
 	//	public static SpeechletResponse getNoResponse(PaInputData inputData) {
@@ -41,28 +82,23 @@ public class OutputHelper {
 	}
 
 	public static SpeechletResponse getNoResponse(PaInputData inputData, SkillContext c) {
-
-		final String locationOutput="The nearest stop to "+  inputData.getLocationName() +" is " + inputData.getStopName()+".";
-		final String allRoutesOutput=" No " + inputData.getDirection() + ", busses are expected at " + inputData.getStopName() + " in the next 30 minutes ";
-		final String singleRoutesOutput=" No " + inputData.getDirection() + ", " + inputData.getRouteID() + " is expected at " + inputData.getStopName() + " in the next 30 minutes  ";
-		final String allRoutesHelpText=CHANGE_MARKER+"to hear predictions for all routes that stop there, say <break time=\"0.25s\" /> Alexa, ask "+GetNextBusSpeechlet.INVOCATION_NAME+" for All Routes";
-
+		SsmlOutputSpeech outputSpeech=new SsmlOutputSpeech();
 		String textOutput="";
 		if (c.needsLocation()){
-			textOutput=locationOutput;
+			textOutput=String.format(LOCATION_SPEECH, inputData.getLocationName(), inputData.getStopName()); 
 		}
 
 		if (c.isAllRoutes()){
-			textOutput+=allRoutesOutput;
+			textOutput+=String.format(NO_ALL_ROUTES_SPEECH, inputData.getDirection(), inputData.getStopName());
 		} else {
-			textOutput+=singleRoutesOutput;
+			textOutput+=String.format(NO_SINGLE_ROUTE_SPEECH, inputData.getDirection(), inputData.getRouteID() , inputData.getStopName());
 		}
 
 		if ((c.needsMoreHelp())&&(!c.isAllRoutes())){
-			textOutput+=allRoutesHelpText;
+			textOutput+=HELP_ALL_ROUTES_SPEECH;
 		}
 
-		SsmlOutputSpeech outputSpeech=new SsmlOutputSpeech();
+		
 		outputSpeech.setSsml("<speak> " + AUDIO_FAILURE + textOutput + "</speak>");
 		return SpeechletResponse.newTellResponse(outputSpeech, buildCard(textOutput));
 
@@ -77,15 +113,15 @@ public class OutputHelper {
 		String textOutput;
 		String speechOutput;
 
-		final String locationOutput="The nearest stop to "+  inputData.getLocationName() +" is " + inputData.getStopName()+". ";
-		final String stopOutput=" At " +inputData.getStopName()+", ";
-		final String allRoutesHelpText=" <break time=\"0.25s\" />  to hear predictions for all routes that stop "+stopOutput+  ", say <break time=\"0.25s\" /> Alexa, ask "+GetNextBusSpeechlet.INVOCATION_NAME+" for All Routes";
+		//final String locationOutput="The nearest stop to "+  inputData.getLocationName() +" is " + inputData.getStopName()+". ";
+		//final String stopOutput=" At " +inputData.getStopName()+", ";
+		//final String allRoutesHelpText=" <break time=\"0.25s\" />  to hear predictions for all routes that stop "+BUSSTOP_SPEECH+  ", say <break time=\"0.25s\" /> Alexa, ask "+GetNextBusSpeechlet.INVOCATION_NAME+" for All Routes";
 
 
 		if (c.needsLocation()){
-			textOutput=locationOutput;
+			textOutput=String.format(LOCATION_SPEECH, inputData.getLocationName(), inputData.getStopName());
 		} else {
-			textOutput=stopOutput;
+			textOutput=String.format(BUSSTOP_SPEECH, inputData.getStopName()) ;
 		}
 		speechOutput = textOutput+"<break time=\"0.1s\" />";
 
@@ -102,23 +138,23 @@ public class OutputHelper {
 			when = results.get(i).getEstimate();
 
 			if (i==0){
-				textOutput += " The " + routeID + " will be arriving in " + when + " minutes ";
-				speechOutput += "The " + routeID + " will be arriving in " + when + " minutes ";
+				textOutput += String.format(FIRST_RESULT_SPEECH,routeID,when);
+				speechOutput += String.format(FIRST_RESULT_SPEECH,routeID,when);
 			} else if (routeID.equals(prevRouteID)){
-				textOutput += " and "+when+ " minutes ";
-				speechOutput += " and "+when+ " minutes ";
+				textOutput += String.format(MORE_RESULTS_SPEECH, when);
+				speechOutput += String.format(MORE_RESULTS_SPEECH, when);
 			} else {
-				textOutput += ".\n The " + routeID + " will be arriving in " + when + " minutes ";
-				speechOutput += "<break time=\"0.25s\" /> The " + routeID + " will be arriving in " + when + " minutes ";
+				textOutput += ".\n "+String.format(FIRST_RESULT_SPEECH,routeID,when);
+				speechOutput += "<break time=\"0.25s\" /> "+String.format(FIRST_RESULT_SPEECH,routeID,when);
 			}
 			prevRouteID=routeID;
 		}
 
 		if ((c.needsMoreHelp())&&(!c.isAllRoutes())){
-			speechOutput+=allRoutesHelpText;
+			speechOutput+=HELP_ALL_ROUTES_SPEECH;
 		}
 		outputSpeech.setSsml("<speak> " + AUDIO_SUCCESS + speechOutput + "</speak>");
-		return SpeechletResponse.newTellResponse(outputSpeech, buildCard(textOutput));
+		return SpeechletResponse.newTellResponse(outputSpeech, buildCard(textOutput, inputData.getLocationLat(), inputData.getLocationLong(), inputData.getStopLat(), inputData.getStopLon()));
 	}
 	/**
 	 * @param s
@@ -126,10 +162,27 @@ public class OutputHelper {
 	 */
 	private static SimpleCard buildCard(String s){
 		SimpleCard card=new SimpleCard();
-		card.setTitle("Pittsburgh Port Authority");
+		card.setTitle(GetNextBusSpeechlet.INVOCATION_NAME);
 		card.setContent(s);
 		return card;
 	}
+        //card with image for successful output
+	private static StandardCard buildCard(String text, String locationLat, String locationLong, double stopLat, double stopLon) {
+            StandardCard card = new StandardCard();
+            card.setTitle("Pittsburgh Port Authority");
+            card.setText(text);
+            Image image = new Image();
+            image.setLargeImageUrl(buildImageURL(locationLat, locationLong, stopLat, stopLon));
+            card.setImage(image);
+            return card;
+        }
+
+    
+    private static String buildImageURL(String locationLat, String locationLong, double stopLat, double stopLon) {
+        //Example: "https://maps.googleapis.com/maps/api/staticmap?size=600x300&maptype=roadmap&key=AIzaSyAOTkkr2SDnAQi8-fohOn4rUinICd-pHVA&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7C40.4390895,-80.0108302&markers=size:mid%7Ccolor:0xff0000%7Clabel:2%7C40.4418137,-80.0077432"
+        String url = "https://maps.googleapis.com/maps/api/staticmap?size=1000x700&maptype=roadmap&key=AIzaSyAOTkkr2SDnAQi8-fohOn4rUinICd-pHVA&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%" + locationLat + "," + locationLong + "&markers=size:mid%7Ccolor:0xff0000%7Clabel:2%7C" + stopLat + "," + stopLon;
+        return url;
+    }
 
 
 
